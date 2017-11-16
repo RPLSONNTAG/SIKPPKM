26821dda990f6d38addc6f37fbd60bbf5d06985f
anare97 <anare97@users.noreply.github.com>
2017-11-16 22:14:21 +0700

    Update c_ManageUser.php

--- application/controllers/c_ManageUser.php
+++ application/controllers/c_ManageUser.php
@@ -9,17 +9,27 @@ class ManageUser extends CI_Controller {
 	}
 
 	public function addUser(){
+		$this->form_validation->set_rules('username', 'username', 'required');
+		$this->form_validation->set_rules('password', 'password', 'required');
+		$this->form_validation->set_rules('email', 'email', 'required');
+		$this->form_validation->set_rules('telp', 'telp', 'required');
+		$this->form_validation->set_rules('NIK', 'NIK', 'required');
+		
 		$username=$this->security->sanitize_filename($_POST['username']);
 		$password=$this->security->sanitize_filename($_POST['password']);
 		$email=$this->security->sanitize_fielname($_POST['email']);
 		$noTelp=$this->security->sanitize_fielname($_POST['telp']);
 		$NIK=$this->security->sanitize_fielname($_POST['NIK']);
-		$work=$this->m_user->addUser($username, $password, $email, $noTelp, $NIK);
+		if($this->form_validation->run()==FALSE){
+			$this->session->set_flashdata();
+		} else {
+			$this->m_user->addUser($username, $password, $email, $noTelp, $NIK);
+		}
 	}
 
 	public function delUser(){
 		$username=$this->security->sanitize_filename($_POST['username']);
-		$work=$this->m_user->delUser($username);
+		$this->m_user->delUser($username);
 	}
 
 }
